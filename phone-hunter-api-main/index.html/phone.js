2699ef61ddc1d32f54f4393eba7817546b28b68b
coder-utshab <ghoshutshob470@gmail.com>
2026-02-06 17:42:47 +0530

    up

--- phone-hunter-api-main/index.html/phone.js
+++
@@ -1,7 +0,0 @@
-const loadPhone = async () =>{
-    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
-    const data = await res.json();
-    const phones = data.data;
-    console.log(phones);
-}
-loadPhone();
